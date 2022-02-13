0ac36d2c93e6d7298d9888a5658044cd75331f84
S-Abbdullah <98264416+S-Abbdullah@users.noreply.github.com>
2022-02-13 22:04:36 +0500

    Function_basic

---
+++ Functions_basic.dart
@@ -0,0 +1,29 @@
+// Functions
+
+// 1. Reusabily
+// 2. Arithmatic operation only on same datatype
+// Escaape  sequence
+// 3. \n New Line
+// 4. \t 8 spaces
+// 5. \a alert
+
+void hi(){
+  int a=10;
+  int b =2;
+  int f = a*b;
+  int s = a+b;
+  print("My name is $f $s holiday \n ok no");
+}
+
+main(List<String> obj) {
+  hi();
+}
+
+//Kamraan Bhai
+// Make fuction that sums (+) 5 num call 1
+
+//Areeb Bhai 
+// Make function that multiply 2 num then /4 call 4
+
+//Faheem Bhai 
+// Make A function that Take number from user + 5
