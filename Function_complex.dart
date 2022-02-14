ed90decd854b0563c3263cca736dd74212d40b46
S-Abbdullah <98264416+S-Abbdullah@users.noreply.github.com>
2022-02-14 22:17:28 +0500

    Function_complex

---
+++ Function_complex.dart
@@ -0,0 +1,27 @@
+// .toString() is use to convert any datatype into string
+// String hi = true.toString();
+// int.parse() is use to convert any datatype into int
+// double.parse() is use to convert any datatype into double
+// /! Null safety ignored
+// Compiler runs the program
+
+Square(double width, int height,){
+  print(width*height);
+}
+
+void main(List<String> args) {
+  Square(5, 5);
+  Square(6, 7);
+  Square(9, 2);
+}
+
+//Kamran Bhai
+// 1. make function then declare 4 value multiple 2 value then divide 2 values and then plus both answers
+// 2. Print "'/Ali        Hello/'"
+
+//Faheem bhai
+// 1. make function then declare 10 value 
+// 2. Search % opertor
+
+//Areeb Bhai
+// 1. Make a function that print persentage of any number from 100 
