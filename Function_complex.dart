519dd4e9fd8b01184201e4b3a8696156392115b0
S-Abbdullah <98264416+S-Abbdullah@users.noreply.github.com>
2022-02-15 22:03:35 +0500

    condition

--- Function_complex.dart
+++ Function_complex.dart
@@ -2,18 +2,22 @@
 // String hi = true.toString();
 // int.parse() is use to convert any datatype into int
 // double.parse() is use to convert any datatype into double
+// \" is use to get (") in output
+// \' is use to get (') in output
+// \\ is use to get (\) in output
+// \Special symbol is use to get (#,$,%,& etc..) in output
 // /! Null safety ignored
 // Compiler runs the program
 
-Square(double width, int height,){
-  print(width*height);
-}
+// Square(double width, int height,){
+//   print(width*height);
+// }
 
-void main(List<String> args) {
-  Square(5, 5);
-  Square(6, 7);
-  Square(9, 2);
-}
+// void main(List<String> args) {
+//   Square(5, 5);
+//   Square(6, 7);
+//   Square(9, 2);
+// }
 
 //Kamran Bhai
 // 1. make function then declare 4 value multiple 2 value then divide 2 values and then plus both answers
@@ -24,4 +28,7 @@ void main(List<String> args) {
 // 2. Search % opertor
 
 //Areeb Bhai
-// 1. Make a function that print persentage of any number from 100 
+// 1. Make a function that print persentage of any number from 100
+void main(List<String> args) {
+  print("\'\"\\\$ Ali\t\tHello\\\'\"");
+}
